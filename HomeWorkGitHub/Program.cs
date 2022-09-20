b4ad4fcc1b1de40464edbb9f50a062ec2b8bee71
«User» <9323354@gmail.com>
2022-09-20 20:59:41 +0300

    Completed task 1

--- HomeWorkGitHub/Program.cs
+++ HomeWorkGitHub/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num1 = (num / 10);
+Console.WriteLine($"Вторая цифра числа {num}: {num1 % 10}");
